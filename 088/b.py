5d763d7f39bd5933716f1994b6dffc889e132cbb
重田 将人 <masato.0928.j@gmail.com>
2020-02-01 09:20:14 +0900

    過去問コミット

---
+++ 088/b.py
@@ -0,0 +1,10 @@
+# B - Card Game for Two
+# https://abc088.contest.atcoder.jp/tasks/abc088_b
+
+n = int(input())
+a = sorted(list(map(int,input().split())), reverse=True)
+
+print(sum(a[::2]) - sum(a[1::2]))
+
+
+
