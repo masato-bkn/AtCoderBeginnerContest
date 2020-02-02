85af69453b89678f23e332b8a7e98577a6bcd0a2
重田 将人 <masato.0928.j@gmail.com>
2020-02-02 09:42:11 +0900

    過去問コミット

--- 085/c.py
+++ 085/c.py
@@ -3,15 +3,15 @@
 
 N, Y = map(int,input().split())
 
-
-# x 1000
+# x 10000
 # y 5000
-# z 10000
+# z 1000
+
 for x in range(N+1):
     for y in range(N-x+1):
-        z = N-x+y
-        if (x*1000)+(y*5000)+(z*10000) == Y:
-            print(f"{str(y)} {str(z)} {str(x)}")
+        z=N-(x+y)
+        if 0<=z<=2000 and (x*10000)+(y*5000)+(z*1000) == Y:
+            print(f"{str(x)} {str(y)} {str(z)}")
             exit()
 
 print("-1 -1 -1")
