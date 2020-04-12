7b1ba762155c888755adccd5f751ad037bd6bd07
重田 将人 <masato.0928.j@gmail.com>
2020-04-12 17:08:21 +0900

    過去問解いた

--- 042/a.py
+++ 042/a.py
@@ -1,16 +1,16 @@
-# A - 和風いろはちゃんイージー / Iroha and Haiku (ABC Edition)
-# https://atcoder.jp/contests/abc042/tasks/abc042_a
+    # A - 和風いろはちゃんイージー / Iroha and Haiku (ABC Edition)
+    # https://atcoder.jp/contests/abc042/tasks/abc042_a
 
-nums = list(map(int,input().split()))
+    nums = list(map(int,input().split()))
 
-for i in [5,7,5]:
-    if i in nums:
-        nums.remove(i)
+    for i in [5,7,5]:
+        if i in nums:
+            nums.remove(i)
+        else:
+            print("NO")
+            exit()
+
+    if len(nums) == 0:
+        print("YES")
     else:
         print("NO")
-        exit()
-
-if len(nums) == 0:
-    print("YES")
-else:
-    print("NO")
